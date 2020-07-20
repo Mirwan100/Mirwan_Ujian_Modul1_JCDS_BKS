44c81de57d718894c2ea26fda38803780de66206
Mirwan100 <takeitsall@gmail.com>
2020-07-20 12:25:25 +0700

    Update Ujian modul1.py

--- Ujian modul1.py
+++ Ujian modul1.py
@@ -27,7 +27,7 @@ def create_phone_number(number):
         phone_number = 'Input 10 digits of number!'
     return phone_number
     
-print(create_phone_number([1,5,3,4,5,6,7,8,9,0]))'''
+print(create_phone_number([1,5,3,4,5,6,7,8,9,0]))
 
 def sort_odd_even(list_num):
     ganjil = []
